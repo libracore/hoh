63ec57e6781be95c704620b97ce249d51b67b948
lasalesi <larsericmueller@gmail.com>
2020-08-25 21:19:32 +0200

    v0.6.0 with calculation

--- hoh/hoh/doctype/kalkulation/kalkulation.js
+++ hoh/hoh/doctype/kalkulation/kalkulation.js
@@ -2,7 +2,46 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Kalkulation', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+	},
+    bemusterung: function(frm) {
+        // change trigger for base item
+        if (frm.doc.bemusterung) {
+            frappe.call({
+                method: 'load_items',
+                doc: frm.doc,
+                args: {
+                    'base_item': frm.doc.bemusterung
+                },
+                callback: function(response) {
+                    console.log("back");
+                   var material = response.message;
+                   console.log(material);
+                   // clear material table
+                   clear_material_rows(frm);
+                   console.log("clear");
+                   // fill material
+                   for (var i = 0; i < material.length; i++) {
+                       var child = cur_frm.add_child('material');
+                        frappe.model.set_value(child.doctype, child.name, 'item', material[i].item);
+                        frappe.model.set_value(child.doctype, child.name, 'qty', material[i].qty);
+                        frappe.model.set_value(child.doctype, child.name, 'rate', material[i].rate);
+                   }
+                   cur_frm.refresh_field('material');
+                }
+            });
+        }
+    }
 });
+
+function clear_material_rows(frm) {
+    // remove all rows
+    var tbl = frm.doc.material || [];
+    var i = tbl.length;
+    while (i--)
+    {
+        cur_frm.get_field("material").grid.grid_rows[i].remove();
+    }
+    cur_frm.refresh();
+}
--- hoh/hoh/doctype/kalkulation/kalkulation.py
+++ hoh/hoh/doctype/kalkulation/kalkulation.py
@@ -3,8 +3,19 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Kalkulation(Document):
-	pass
+    def load_items(self, base_item):
+        root = frappe.get_doc("Bemusterung", base_item)
+        material = []
+        for item in root.items:
+            item_master_data = frappe.get_doc("Item", item.item_code)
+            cost = item_master_data.last_purchase_rate or item.valuation_rate or item_master_data.valuation_rate
+            material.append({
+                'item': item.item_code,
+                'qty': item.qty,
+                'rate': cost
+            })
+        return material
