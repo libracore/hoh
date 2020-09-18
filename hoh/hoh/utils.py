2fe7a11ce2ede1ac83954f2a2d1a4d1742342028
nanipi <nicole.gret@libracore.com>
2020-09-18 10:28:08 +0200

    buttonadd several prices

--- hoh/hoh/utils.py
+++ hoh/hoh/utils.py
@@ -20,3 +20,42 @@ def get_batch_info(item_code):
     
     data = frappe.db.sql(sql_query, as_dict=1)
     return data
+
+"""
+   :params:
+   item_code_pattern: "CAI%": item_code mask to find items
+   price_list: name of target price list
+   rate: set this rate
+"""
+@frappe.whitelist()
+def bulk_set_prices(item_code_pattern, price_list, rate):
+    # find applicable items
+    sql_query = """SELECT `name` 
+                   FROM `tabItem`
+                   WHERE `tabItem`.`item_code` LIKE '{pattern}' 
+                     AND `tabItem`.`disabled` = 0;""".format(pattern=item_code_pattern)
+    applicable_items = frappe.db.sql(sql_query, as_dict=True)
+    updated_item_prices = []
+    # loop through items
+    for item in applicable_items:
+        # check if item price exists
+        item_prices = frappe.get_all("Item Price", filters={'item_code': item['name'], 'price_list': price_list}, fields=['name'])
+        if item_prices:
+            # update existing price record
+            item_price = frappe.get_doc("Item Price", item_prices[0]['name'])
+            item_price.price_list_rate = rate
+            item_price.save()
+            updated_item_prices.append(item_price.name)
+        else:
+            # create new item price record
+            new_item_price = frappe.get_doc({
+                'doctype': 'Item Price',
+                'item_code': item['name'],
+                'price_list': price_list,
+                'price_list_rate': rate
+            })
+            new_record = new_item_price.insert()
+            updated_item_prices.append(new_record.name)
+    # write changes to database
+    frappe.db.commit()
+    return updated_item_prices
