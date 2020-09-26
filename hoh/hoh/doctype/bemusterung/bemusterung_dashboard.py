f7a023aa7ec04912509f9dbac0a72ebee6da440f
lasalesi <larsericmueller@gmail.com>
2020-09-26 23:55:32 +0200

    added likes

--- hoh/hoh/doctype/bemusterung/bemusterung_dashboard.py
+++ hoh/hoh/doctype/bemusterung/bemusterung_dashboard.py
@@ -2,12 +2,16 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-	return {
-		'fieldname': 'bemusterung',
-		'transactions': [
-			{
-				'label': _('Kalkulation'),
-				'items': ['Kalkulation']
-			}
-		]
-	}
+    return {
+        'fieldname': 'bemusterung',
+        'transactions': [
+            {
+                'label': _('Kalkulation'),
+                'items': ['Kalkulation']
+            },
+            {
+                'label': _('Online Catalogue'),
+                'items': ['Online Catalogue Like Trace']
+            }
+        ]
+    }
--- hoh/hoh/doctype/online_catalogue_like_trace/online_catalogue_like_trace.py
+++ hoh/hoh/doctype/online_catalogue_like_trace/online_catalogue_like_trace.py
@@ -3,8 +3,19 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class OnlineCatalogueLikeTrace(Document):
-	pass
+    pass
+
+@frappe.whitelist()
+def add_like(user, bemusterung, like=1):
+    new_comment = frappe.get_doc({
+        'doctype': "Online Catalogue Like Trace",
+        'user': user,
+        'bemusterung': bemusterung,
+        'like': like
+    })
+    new_comment.insert()
+    return
