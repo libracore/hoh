3de1d052e24152e3f909d31244c300354a13b102
lasalesi <larsericmueller@gmail.com>
2020-10-26 21:55:52 +0100

    time recording on work order

--- hoh/public/js/work_order.js
+++ hoh/public/js/work_order.js
@@ -7,7 +7,22 @@ frappe.ui.form.on('Work Order', {
                 cur_frm.set_value("ausruestung_fertig", 1);
                 cur_frm.save_or_update();
             }).addClass("btn-primary");
-        } 
+        }
+        if (frm.doc.docstatus < 2) {
+            var datestamp = frappe.datetime.now_date() + " " + frappe.datetime.now_time();
+            if ((frm.doc.nutzungen.length > 0) && (!frm.doc.nutzungen[frm.doc.nutzungen.length - 1].end)) {
+                frm.add_custom_button(__('Stop'), function() {
+                    frappe.model.set_value(frm.doc.nutzungen[frm.doc.nutzungen.length - 1].doctype, frm.doc.nutzungen[frm.doc.nutzungen.length - 1].name, 'end', datestamp);
+                    cur_frm.save_or_update();
+                }).addClass("btn-warning");
+            } else {
+                frm.add_custom_button(__('Start'), function() {
+                    var child = cur_frm.add_child('nutzungen');
+                    frappe.model.set_value(child.doctype, child.name, 'start', datestamp);
+                    cur_frm.save_or_update();
+                }).addClass("btn-warning");
+            }
+        }
     },
     production_item: function(frm) {
         // change on production item --> fetch related data fields
@@ -15,13 +30,17 @@ frappe.ui.form.on('Work Order', {
             fetch_item_details(frm);
         }
     },
-    before__save: function(frm) {
+    before_save: function(frm) {
         if (!frm.doc.stoff) {
             fetch_item_details(frm);
         }
         // calculate total machine hours
         var machine_hours = 0;
         for (var i = 0; i < frm.doc.nutzungen.length; i++) {
+            if ((frm.doc.nutzungen[i].end) && (frm.doc.nutzungen[i].start)) {
+                var duration = (new Date(frm.doc.nutzungen[i].end) - new Date(frm.doc.nutzungen[i].start)) / 3600000;
+                frappe.model.set_value(frm.doc.nutzungen[i].doctype, frm.doc.nutzungen[i].name, 'maschinenstunden', duration);
+            }
             machine_hours += frm.doc.nutzungen[i].maschinenstunden;
         }
         cur_frm.set_value('summe_maschinenstunden', machine_hours);
