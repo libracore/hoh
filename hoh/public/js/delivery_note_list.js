652848184af58c93f1744bbdb4846e2216659551
lasalesi <larsericmueller@gmail.com>
2021-01-25 21:22:59 +0100

    adding delivery note labels

---
+++ hoh/public/js/delivery_note_list.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2020-2021, libracore and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings['Delivery Note'] = {
+    onload: function(listview) {
+        listview.page.add_menu_item(__("Create Label"), function() {
+            var selected = listview.get_checked_items();
+            create_label(selected);
+        });
+    }
+};
+
+function create_label(selected) {
+    // compile items list
+    var selected_items = [];
+    for (var i = 0; i < selected.length; i++) {
+        selected_items.push(selected[i].name);
+    }
+    // retrieve pdf
+    if (selected_items.length > 0) {
+        // html-content of the label
+        var url = "/api/method/hoh.hoh.labels.get_delivery_note_label"  
+                + "?selected_delivery_notes=" + encodeURIComponent("'" + selected_items.join("','") + "'");
+        var w = window.open(
+             frappe.urllib.get_full_url(url)
+        );
+        if (!w) {
+            frappe.msgprint(__("Please enable pop-ups")); return;
+        }
+    } else {
+        frappe.msgprint(__("Please select items")); return;
+    }
+}
