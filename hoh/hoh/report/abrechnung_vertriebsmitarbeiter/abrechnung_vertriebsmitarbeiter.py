100c64d5ba8e82e474d026104d97e103f8dba2ae
nanipi <nicole.gret@libracore.com>
2021-02-09 08:50:06 +0100

    structural changes in report

--- hoh/hoh/report/abrechnung_vertriebsmitarbeiter/abrechnung_vertriebsmitarbeiter.py
+++ hoh/hoh/report/abrechnung_vertriebsmitarbeiter/abrechnung_vertriebsmitarbeiter.py
@@ -21,47 +21,44 @@ def get_data(filters):
     data = []
     from_date = getdate(filters.from_date)
     to_date = getdate(filters.to_date)
-    sinvs = frappe.db.sql("""SELECT 
-    `tabSales Invoice`.`name` AS `name`, 
-    `tabSales Invoice`.`base_net_total` AS `base_net_total`, 
-    `tabSales Invoice`.`posting_date` AS `posting_date`,
-    `tabSales Team`.`sales_person` AS `sales_person`
-    FROM 
-    `tabSales Invoice` 
-    LEFT JOIN
-    `tabSales Team` ON `tabSales Invoice`.`name` = `tabSales Team`.`parent`
-    WHERE 
-    `tabSales Team`.`sales_person` = '{sales_person}' AND `tabSales Invoice`.`posting_date` BETWEEN '{from_date}' AND '{to_date}'""".format(sales_person=filters.sales_person, from_date=from_date, to_date=to_date), as_dict=True)
-    sum_base_net_total = 0
-    sum_deduction = 0
-    for sinv in sinvs:
-        sum_base_net_total += sinv.base_net_total
-        deduction = frappe.db.sql("""SELECT SUM(`debit`) AS `debit` FROM `tabGL Entry` WHERE `account` = '7300 - Transporte durch Dritte - HOH' AND `tabGL Entry`.`posting_date` BETWEEN '{from_date}' AND '{to_date}'""".format(sinv=sinv.name, from_date=from_date, to_date=to_date), as_dict=True)
-        if len(deduction) > 0:
-            if deduction[0].debit:
-                sum_deduction += float(deduction[0].debit)
+    # get net revenue
+    sum_net_base_total = frappe.db.sql("""SELECT 
+        SUM(`tabSales Invoice`.`base_net_total`) AS `base_net_total`
+    FROM `tabSales Invoice` 
+    LEFT JOIN `tabSales Team` ON `tabSales Invoice`.`name` = `tabSales Team`.`parent`
+    WHERE `tabSales Team`.`sales_person` = '{sales_person}' AND `tabSales Invoice`.`docstatus` = '1'
+      AND `tabSales Invoice`.`posting_date` BETWEEN '{from_date}' AND '{to_date}'
+    """.format(sales_person=filters.sales_person, from_date=from_date, to_date=to_date), as_dict=True)
+    net_revenue = sum_net_base_total[0]['base_net_total'] or 0
+    data.append({
+        'platzhalter': _("Nettoumsatz Total"),
+        'net_amount': net_revenue
+    })
 
-    _data = []
-    _data.append("Nettoumsatz Total")
-    _data.append(sum_base_net_total)
-    _data.append("")
-    data.append(_data)
-    _data = []
-    _data.append("Transportkosten Total")
-    _data.append(sum_deduction)
-    _data.append("")
-    data.append(_data)
-    _data = []
-    _data.append("Zwischensumme")
-    zwischensumme = sum_base_net_total - sum_deduction
-    _data.append(zwischensumme)
-    _data.append("")
-    data.append(_data)
-    _data = []
-    _data.append("Kommission")
-    kommission = zwischensumme * 0.03
-    _data.append(kommission)
-    _data.append("")
-    data.append(_data)
+    # get transport deduction
+    deduction = frappe.db.sql("""SELECT SUM(`debit`) AS `debit` 
+      FROM `tabGL Entry` 
+      WHERE `account` = '7300 - Transporte durch Dritte - HOH' 
+        AND `tabGL Entry`.`posting_date` BETWEEN '{from_date}' AND '{to_date}'
+    """.format(from_date=from_date, to_date=to_date), as_dict=True)
+    deductions = deduction[0]['debit'] or 0
+    data.append({
+        'platzhalter': _("Transportkosten Total"),
+        'net_amount': deductions
+    })
+
+    # intermediate sum
+    intermediate = net_revenue - deductions
+    data.append({
+        'platzhalter': _("Zwischensumme"),
+        'net_amount': intermediate
+    })
+
+    # commission
+    commission = 0.03 * intermediate
+    data.append({
+        'platzhalter': _("Kommission"),
+        'net_amount': commission
+    })
     
     return data
