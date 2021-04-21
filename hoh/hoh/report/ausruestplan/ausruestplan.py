96568fc04a3dbd7f8baf04f987d17408498daae7
lasalesi <larsericmueller@gmail.com>
2021-04-21 14:56:24 +0200

    bigfix multiple so items

--- hoh/hoh/report/ausruestplan/ausruestplan.py
+++ hoh/hoh/report/ausruestplan/ausruestplan.py
@@ -41,7 +41,8 @@ def get_data(filters):
          (SELECT CONCAT(ROUND(`tabSales Order Item`.`anzahl`, 0), " x ", ROUND(`tabSales Order Item`.`verkaufseinheit`, 1), " ", `tabSales Order Item`.`uom`) 
             FROM `tabSales Order Item`
             WHERE `tabSales Order Item`.`item_code` = `tabWork Order`.`production_item`
-              AND `tabSales Order Item`.`parent` = `tabWork Order`.`sales_order`) AS `qty_full`,
+              AND `tabSales Order Item`.`parent` = `tabWork Order`.`sales_order`
+            LIMIT 1) AS `qty_full`,
          `tabWork Order`.`stock_uom` AS `uom`,
          `tabWork Order`.`anmerkung` AS `anmerkung`,
          (SELECT `finish_step` 
