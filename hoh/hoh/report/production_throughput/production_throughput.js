b0b726b82ff5bdf94b82d6f6b2a8244acd4153ae
nanipi <nicole.gret@libracore.com>
2021-05-18 10:53:11 +0200

    2 new reports and fieldchanges in workorder childtbl to add schicht

---
+++ hoh/hoh/report/production_throughput/production_throughput.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2016, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Production Throughput"] = {
+	"filters": [
+		{
+			"fieldname": "year",
+			"label": __("Year"),
+			"fieldtype": "Int",
+			"default": new Date().getFullYear(),
+			"reqd": 1
+		}
+	],
+    "onload": (report) => {
+        report.page.add_inner_button(__('Stickplan'), function () {
+           window.location.href="/desk#query-report/Stickplan";
+        }, __("Open") );
+    },
+};
